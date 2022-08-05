5ae7191969196790ca4382c03b480601c80202ec
Serg <serg@localhost>
2022-08-05 18:56:10 +0300

    task №3 done

--- task_3/solution.js
+++ task_3/solution.js
@@ -2,7 +2,7 @@ function renderCartItem(item) {
     const product = item;
 
     // Задание №3.1. Формирование строки корзины
-
+    
     // product - объект вида {id: id, title: title, count: count, price: price}
     // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
     // id - идентификатор
@@ -22,6 +22,10 @@ function renderCartItem(item) {
     // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
     // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
     // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
+
+    let itemCountText = "" + product.count + " × " + product.price + " ₽ = ";
+    let sum = product.price * product.count;
+    itemCountText = itemCountText + sum + " ₽";
 
     // Конец решения задания №3.1.
 
